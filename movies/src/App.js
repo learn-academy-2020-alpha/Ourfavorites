91a0eb1a4cb71aa77943d6f43ff6a506a86a86b9
Julia <jlilianacollins@gmail.com>
2020-03-23 13:37:26 -0700

    working router

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom"
 import './App.css';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Television from './pages/Television';
+import NotFound from './pages/NotFound';
 
-function App() {
+
+const App = () => {
   return (
-   <>
-   </>
+   <Router>
+     <div>
+       <h1>This is our router</h1>
+       <nav>
+         <ul>
+           <li>
+             <Link to="/">Home</Link>
+           </li>
+           <li>
+             <Link to="/movie">Movie</Link>
+           </li>
+           <li>
+             <Link to="/television">Television</Link>
+           </li>
+         </ul>
+       </nav>
+       <Switch>
+         <Route path="/movie" component={ Movie } />
+         <Route path="/television" component={ Television } />
+         <Route path="/" exact component={ Home } />
+         <Route component={ NotFound } />
+
+
+
+       </Switch>
+     </div>
+   </Router>
   );
 }
 
---
+++ movies/src/pages/Television.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Television = () => {
+   return(
+     <>
+       <h1>Television</h1>
+     </>
+   )
+}
+export default Television
